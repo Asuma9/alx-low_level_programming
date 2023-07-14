e7270a017ffe6d96692b5f6c154a6fd18d085b0b
Asuma9 <paulasuma10@gmail.com>
2023-07-14 14:36:35 -0700

    Program that prints all possible combinations of single-digit numbers with commas using puchar

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+  *main - main function
+  *
+  *return: 0 always
+  */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		if (i == 9)
+			putchar(i + '0');
+		else
+		{
+		putchar(i + '0');
+		putchar(',');
+		putchar(' ');
+		}
+	}
+
+	return (0);
+}
