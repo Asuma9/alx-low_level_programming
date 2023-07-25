cb95ce94acef015c177fc68e4fa7ca2b2373ceb3
Asuma9 <paulasuma10@gmail.com>
2023-07-25 07:37:37 +0300

    function that prints n elements of an array of integers, followed by a new line.

---
+++ 0x05-pointers_arrays_strings/8-main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * main - program that prints n elements of an array of integers
+ * Return: 0 always
+ */
+int main(void)
+{
+	int array[5];
+
+	array[0] = 98;
+	array[1] = 402;
+	array[2] = -198;
+	array[3] = 298;
+	array[4] = -1024;
+	print_array(array, 5);
+	return (0);
+}
