872305812c0b42890e1bd31213c1ccbcbfd9ab95
Asuma9 <paulasuma10@gmail.com>
2023-07-25 06:35:31 +0300

    Function that prints even characters starting from the first character

---
+++ 0x05-pointers_arrays_strings/6-main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * main - program to print every character of a string
+ * Return: 0 always
+ */
+int main(void)
+{
+	char *str;
+
+	str = "0123456789";
+	puts2(str);
+	return (0);
+}
