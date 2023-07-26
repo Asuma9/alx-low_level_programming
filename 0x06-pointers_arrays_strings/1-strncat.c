c70d31c385a198392cb61f6bcaa38da40cefae11
Asuma9 <paulasuma10@gmail.com>
2023-07-26 19:38:13 +0300

    function that concatenates two strings without null byte.

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _strncat - function to concatenate two strings
+ * @dest: destination pointer
+ * @src: source pointer
+ * @n: parameter to be tested
+ * Return: dest
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int i;
+	int len = strlen(dest);
+
+	for (i = 0; i < n && *src != '\0'; i++)
+	{
+		dest[len + i] = *src;
+		src++;
+	}
+	dest[len + i] = '\0';
+	return (dest);
+}
