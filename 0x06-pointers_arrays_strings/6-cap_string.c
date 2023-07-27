530817b0f6d09ea75f1fc1779fb01a6f80fa733b
Asuma9 <paulasuma10@gmail.com>
2023-07-27 08:00:11 +0300

    function that capitalizes all words of a string.

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -7,7 +7,7 @@
 
 char *cap_string(char *s)
 {
-	int i =0;
+	int i = 0;
 	/*int a = {32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};*/
 
 	while (s[i])
@@ -18,7 +18,7 @@ char *cap_string(char *s)
 		}
 		if (s[i - 1] == 32 ||
 				s[i - 1] == 9 ||
-				s[i -1] == 10 ||
+				s[i - 1] == 10 ||
 				s[i - 1] == 44 ||
 				s[i - 1] == 59 ||
 				s[i - 1] == 46 ||
@@ -30,7 +30,7 @@ char *cap_string(char *s)
 				s[i - 1] == 123 ||
 				s[i - 1] == 125 ||
 				i == 0)
-			s[i] -=32;
+			s[i] -= 32;
 		i++;
 	}
 	return (s);
