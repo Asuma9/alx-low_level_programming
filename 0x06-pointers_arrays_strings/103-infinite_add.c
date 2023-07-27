8dca56af473f8a558d1f6e8444122c551cb34ab2
Asuma9 <paulasuma10@gmail.com>
2023-07-27 21:30:02 +0300

    so that the program prints a[2] = 98, followed by a new line.

---
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * infinite_add - function to add two numbers
+ * @n1: para 1
+ * @n2: para 2
+ * @r: para 3
+ * @size_r: para 4
+ * Return: 0 always
+ */
+char *infinite_add(char *n2, char *n2, char *r, int size_r)
+{
+	int i, j;
+	
+}
