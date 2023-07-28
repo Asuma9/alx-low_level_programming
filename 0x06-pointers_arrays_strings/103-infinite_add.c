c5badfdea8a9637599c97670c55143680827f605
Asuma9 <paulasuma10@gmail.com>
2023-07-28 23:17:30 +0300

    function that adds two numbers.

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,5 +1,30 @@
 #include "main.h"
 /**
+ * rev_string - reverse array
+ * @n: integer params
+ * Return: 0
+ */
+void rev_string(char *n)
+{
+	int i = 0;
+	int j = 0;
+	char temp;
+
+	while (*(n + i) != '\0')
+	{
+		i++;
+	}
+	i--;
+
+	for (j = 0; j < i; j++, i--)
+	{
+		temp = *(n + i);
+		*(n + j) = *(n + i);
+		*(n + i) = temp;
+	}
+}
+
+/**
  * infinite_add - function to add two numbers
  * @n1: para 1
  * @n2: para 2
