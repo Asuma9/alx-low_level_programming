80e8382550f12413f2a421f36aaeb61a7baa6541
Asuma9 <paulasuma10@gmail.com>
2023-08-05 20:53:50 +0300

    program that prints its name, followed by a new line.

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -3,13 +3,12 @@
 /**
  * main - function to print a its name followed with new line
  * @argc: arguments count
- * __attribute__((unused)) - ignore flagging of unused arguments
  * @argv: array vector
  * Return: 0 always
  */
 
 int main(int argc __attribute__((unused)), char *argv[])
 {
-	printf("Name is = %s\n", *argv);
+	printf("%s\n", *argv);
 	return (0);
 }
