2c8d5062e9872cd3ce745347ef75acee7997cc07
Asuma9 <paulasuma10@gmail.com>
2023-09-05 22:50:34 +0300

    program that copies the content of a file to another file.

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,6 +1,9 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#define READ_ERR "Error: Can't read from file %s\n"
+#define WRITE_ERR "Error: Can't write to %s\n"
+
 #include <stdarg.h>
 #include <stdio.h>
 #include <fcntl.h>
@@ -12,8 +15,6 @@ ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 void error_file(int file_from, int file_to, char *argv[]);
-
-
 
 
 #endif /* MAIN_H */
