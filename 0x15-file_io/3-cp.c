d83fc19be6c27887ae64e571f7d77b8553d3f2a0
Asuma9 <paulasuma10@gmail.com>
2023-09-05 22:56:21 +0300

    program that copies the content of a file to another file.

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -7,7 +7,7 @@
  */
 int main(int argc, char *argv[])
 {
-	int file_from, file_to, error_close;
+	int file_from, file_to, err_close;
 	ssize_t nchars, cwr;
 	char buffer[1024];
 
@@ -16,9 +16,11 @@ int main(int argc, char *argv[])
 		dprintf(STDERR_FILENO, "%s\n", "usage: cp file_from file_to");
 		exit(97);
 	}
+
 	file_from = open(argv[1], O_RDONLY);
 	file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC | O_APPEND, 0664);
 	error_file(file_from, file_to, argv);
+	
 	nchars = 1024;
 	while (nchars == 1024)
 	{
@@ -29,14 +31,16 @@ int main(int argc, char *argv[])
 		if (cwr == -1)
 			error_file(0, -1, argv);
 	}
-	error_close = close(file_from);
-	if (error_close == -1)
+	
+	err_close = close(file_from);
+	if (err_close == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
 		exit(100);
 	}
-	error_close = close(file_to);
-	if (error_close == -1)
+
+	err_close = close(file_to);
+	if (err_close == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
 		exit(100);
