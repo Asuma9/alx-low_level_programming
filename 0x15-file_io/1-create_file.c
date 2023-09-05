eda0f2fe9b98495b773b8ba9a484a606bf26196a
Asuma9 <paulasuma10@gmail.com>
2023-09-05 05:36:13 +0300

    function that creates a file.

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -28,7 +28,7 @@ if (filename == NULL)
 return (-1);
 
 /* open filedes 4 writing/ create if doesnt exist/ truncate if does */
-filedes = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0666);
+filedes = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 if (filedes == -1)
 return (-1);
 
