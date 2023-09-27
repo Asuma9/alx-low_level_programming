a9a7aa94d00c8e9b482ea64c8d7550ef86918bdd
Asuma9 <paulasuma10@gmail.com>
2023-09-27 06:50:20 +0300

    Deleted bit files

---
+++ alternative/0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ **get_endianness - function checks endianness
+ **void: takes no argument
+ **Return: 0 (big endian), 1 (little endian)
+ */
+int get_endianness(void)
+{
+	unsigned int i = 0;
+	char *c;
+
+	c = (char *) &i; /* point to the first byte of int i */
+
+	if (*c == 1) /* 1st byte looks like 0x01 */
+	{
+		return (0);
+	}
+	else /* 1st byte looks like 0x00 */
+		return (1);
+
+}
