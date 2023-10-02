53e0b66b730b44d0a16e90ddef263fa604e3c0aa
Asuma9 <paulasuma10@gmail.com>
2023-10-02 19:25:37 +0300

    function that reads a text file and prints it to the POSIX standard output.

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -9,25 +9,21 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int filedes;
+	int filedes = open(filename, O_RDONLY);
 	char *buffer;
 	ssize_t rd_bytes, wr_bytes;
 
 	if (filename == NULL)
 		return (0);
-
-	filedes = open(filename, O_RDONLY);
 	if (filedes == -1)
 		return (0);
 
-	/*Allocate mem for the buffer to read */
 	buffer = malloc(sizeof(char) * letters + 1); /*+1 for the '\0' */
 	if (buffer == NULL)
 	{
 		close(filedes); /*close fd b4 returning */
 		return (0);
 	}
-
 	/* Read specified no of letters 4rm the file */
 	rd_bytes = read(filedes, buffer, letters);
 	if (rd_bytes == -1)
@@ -37,22 +33,16 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		return (0);
 	}
 
-	/*Null-terminate the buffer */
-	buffer[letters + 1] = '\0';
-	
-
-	/* write read data to standard output */
-	wr_bytes = write(STDOUT_FILENO, buffer, rd_bytes);
+	buffer[letters] = '\0';/*null-terminate string */
+	wr_bytes = write(STDOUT_FILENO, buffer, rd_bytes); /* output */
 	if (wr_bytes == -1)
 	{
 		free(buffer);
 		close(filedes);
 		return (0);
 	}
-
-	/* clean up alloc mem and close opened file */
-	free(buffer);
-	close(filedes);
+	free(buffer); /* free buffer */
+	close(filedes); /* close open file */
 
 	return (rd_bytes);
 }
